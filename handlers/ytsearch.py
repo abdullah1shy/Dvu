fb3148acecd1618197db6341e39411663df83432
abdullah1shy <101516117+abdullah1shy@users.noreply.github.com>
2022-07-03 18:32:23 +0300

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -16,7 +16,7 @@ import pyrogram
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-@app.on_message(pyrogram.filters.command(["search"]))
+@app.on_message(pyrogram.filters.command(["بحث"]))
 async def ytsearch(_, message: Message):
     await message.delete()
     try:
@@ -24,16 +24,16 @@ async def ytsearch(_, message: Message):
             await message.reply_text("» ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴀʀᴄʜ ʙᴀʙʏ!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🔎")
+        m = await message.reply_text("جارً البحث")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
         while i < 4:
-            text += f"📌 ᴛɪᴛʟᴇ : {results[i]['title']}\n"
-            text += f"⏱ ᴅᴜʀᴀᴛɪᴏɴ : {results[i]['duration']}\n"
-            text += f"👀 ᴠɪᴇᴡs : {results[i]['views']}\n"
-            text += f"📣 ᴄʜᴀɴɴᴇʟ : {results[i]['channel']}\n"
-            text += f"🔗 ʟɪɴᴋ : https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"📌 عنوان : {results[i]['title']}\n"
+            text += f"⏱ المدة : {results[i]['duration']}\n"
+            text += f"👀 المشاهدات : {results[i]['views']}\n"
+            text += f"📣 القناة : {results[i]['channel']}\n"
+            text += f"🔗 الرابط : https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
